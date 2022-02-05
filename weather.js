c3af8b0ac511a0498f3c5deac49c695c8d8cfb9e
lan21005 <lan21005@byui.edu>
2022-02-04 23:33:01 -0500

    update

---
+++ weather.js
@@ -0,0 +1,17 @@
+const temp = parseFloat(document.querySelector('.temperatuer').textContent);
+const wind = parseFloat(document.querySelector('.windSpeed').textContent);
+
+if (temp <= 50 && wind >3) {
+wchill = windChill(temp,wind);
+}
+else {
+wchill = 'N/A';
+}
+
+function windChill(temp,wind){
+let f = 35.74 + (temp * 0.6215)
+- (35.75 * Math.pow(wind, 0.16))
++ (0.4275 * temp * Math.pow(wind, 0.16));
+return f.toFixed(0);
+}
+document.querySelector('windchill').innerHTML = '${wchill}F';
